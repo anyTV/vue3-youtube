5527e643b4dc7f8fd23dc480ab54fbd36cb17d20
Sherwin Ferrer <sherwin.ferrer@freedom.tm>
2025-03-27 19:06:22 +0800

    fix: refactor player

--- src/components/YouTube.vue
+++ src/components/YouTube.vue
@@ -5,46 +5,70 @@
 </template>
 
 <script setup lang="ts">
-import { StyleValue, computed, onBeforeUnmount, onMounted, readonly, ref, watch } from 'vue';
+import {
+    StyleValue,
+    computed,
+    onBeforeUnmount,
+    onMounted,
+    readonly,
+    ref,
+    watch,
+    onBeforeMount
+} from 'vue';
 // issues occurred while trying to import 'get-youtube-id' when using Youtube.vue as a thirdparty module
 //import getYouTubeID from 'get-youtube-id';
 import { PlayerVars } from './types';
 import { useAPI } from './api';
 
-interface Window {
-    onYouTubeIframeAPIReadyResolvers?: { (): void }[]
-    onYouTubeIframeAPIReady?: { (): void }
+declare global {
+    interface Window {
+        onYouTubeIframeAPIReadyResolvers?: { (): void }[]
+        onYouTubeIframeAPIReady?: { (): void }
+
+        hbYoutubeIframeAttempts?: number
+
+        YT?: {
+            loaded?: boolean
+            loading?: boolean
+        }
+
+        YTConfig?: {}
+
+        yt_embedsEnableIframeApiSendFullEmbedUrl?: boolean
+        yt_embedsEnableAutoplayAndVisibilitySignals?: boolean
+
+        hbGlobalResolvers?: PromiseWithResolvers<boolean>
+
+        hbComponentLoading?: boolean
+    }
 }
 
-interface Props {
-    src?: string
+const props = withDefaults(defineProps<{
+    src: string
     height?: number | string
     width?: number | string
     host?: string
     vars?: PlayerVars,
-    strictSrc?: boolean
-}
+}>(), {
+    height: 360,
+    width: 640,
+    host: 'https://www.youtube.com',
+});
 
-interface Emits {
+const emits = defineEmits<{
     (event: 'ready', e: any): void
     (event: 'state-change', e: any): void
     (event: 'playback-quality-change', e: any): void
     (event: 'playback-rate-change', e: any): void
     (event: 'error', e: any): void
-    (event: 'api-error', e: any): void
-    (event: 'api-change', e: any): void
-    (event: 'api-load'): void
     (event: 'autoplay-blocked', e: any): void
-}
 
-const props = withDefaults(defineProps<Props>(), {
-    height: 360,
-    width: 640,
-    host: 'https://www.youtube.com',
-    strictSrc: false
-});
+    (event: 'api-error', e: any): void
 
-const emits = defineEmits<Emits>();
+    (event: 'api-change', e: any): void
+
+    (event: 'api-load'): void
+}>();
 
 const wrapperStyle = computed<StyleValue>(() => ({
     width: `${props.width}px`,
@@ -52,117 +76,217 @@ const wrapperStyle = computed<StyleValue>(() => ({
     position: 'relative',
 }));
 
-const youtube = ref<HTMLElement>();
-const player = ref<YT.Player>();
-const initiated = ref(false);
-const ready = ref(false);
+function usePlayer() {
+    const youtubeDiv = ref<HTMLElement>();
+    const youtubePlayer = ref<YT.Player>();
+    const youtubeIframeAPIReady = ref(false);
 
-function initPlayer(div: HTMLElement) {
-    if (!window.YT) {
-        emits('api-error', new ErrorEvent('YouTube API does not exist'));
-        return;
-    }
-    else {
-        emits('api-load');
-    }
+    const initiated = ref(false);
+    const ready = ref(false);
 
-    if (props.strictSrc && !props.src) {
-        // do not initialize player if src is empty and strict-src is set to true
-        return;
-    }
+    const youtubeIframeScriptId = 'youtube-iframe-js-api-script';
+    const youtubeWidgetScriptId = 'www-widgetapi-script';
 
-    initiated.value = true
-    // @ts-ignore
-    // eslint-disable-next-line no-undef
-    player.value = new YT.Player(div, {
-        height: props.height,
-        width: props.width,
-        videoId: props.src,
-        host: props.host,
-        playerVars: props.vars,
-        events: {
-            onReady: (e: any) => {
-                ready.value = true
-                setTimeout(() => emits('ready', e))
-            },
-            onStateChange: (e: any) => emits('state-change', e),
-            onPlaybackQualityChange: (e: any) => emits('playback-quality-change', e),
-            onPlaybackRateChange: (e: any) => emits('playback-rate-change', e),
-            onError: (e: any) => emits('error', e),
-            onApiChange: (e: any) => emits('api-change', e),
-            onAutoplayBlocked: (e: any) => {
-                // NOTE: avoid calling mute() inside of onReady()
-                //       calling mute() inside of onReady() causes onAutoplayBlocked to not trigger, somehow
-                emits('autoplay-blocked', e);
-            },
-        },
-    })
-}
-
-function on_mounted(div: HTMLElement) {
-    let resolver = () => {};
-    const promise = new Promise((resolve: (reason?: any) => void) => resolver = resolve);
-    promise.then(() => initPlayer(div));
-
-    if (!(window as Window).onYouTubeIframeAPIReadyResolvers) {
-        (window as Window).onYouTubeIframeAPIReadyResolvers = []
-    }
-    if (!(window as Window).onYouTubeIframeAPIReady) {
-        (window as Window).onYouTubeIframeAPIReady = () => {
-            // eslint-disable-next-line no-unused-expressions
-            (window as Window).onYouTubeIframeAPIReadyResolvers?.forEach((resolver: Function) => {
-                resolver()
-            })
+    function destroy () {
+        if (youtubePlayer.value) {
+            youtubePlayer.value.destroy();
         }
     }
 
-    const id = 'youtube-iframe-js-api-script'
-    let tag = document.getElementById(id) as HTMLScriptElement
-    if (!tag) {
-        (window as Window).onYouTubeIframeAPIReadyResolvers?.push(resolver);
-        tag = document.createElement('script')
-        tag.id = id
-        tag.src = 'https://www.youtube.com/iframe_api'
-        tag.onerror = (e) => emits('api-error', e);
-        const firstScriptTag = document.getElementsByTagName('script')[0]
+    const { promise, resolve, reject } = Promise.withResolvers();
+
+    window.hbGlobalResolvers = Promise.withResolvers();
+
+    async function createPlayer() {
+        try {
+            await promise;
+
+            const playerConfig = {
+                height: props.height,
+                width: props.width,
+                videoId: props.src,
+                host: props.host,
+                playerVars: props.vars,
+                events: {
+                    onReady: (e: any) => {
+                        ready.value = true;
+                        setTimeout(() => emits('ready', e));
+                    },
+                    onStateChange: (e: any) => emits('state-change', e),
+                    onPlaybackQualityChange: (e: any) => emits('playback-quality-change', e),
+                    onPlaybackRateChange: (e: any) => emits('playback-rate-change', e),
+                    onError: (e: any) => emits('error', e),
+                    onApiChange: (e: any) => emits('api-change', e),
+                    onAutoplayBlocked: (e: any) => {
+                        // NOTE: avoid calling mute() inside of onReady()
+                        //       calling mute() inside of onReady() causes onAutoplayBlocked to not trigger, somehow
+                        emits('autoplay-blocked', e);
+                    },
+                },
+            };
+
+            youtubePlayer.value = new YT.Player(youtubeDiv.value!, playerConfig);
+        } catch (error) {
+
+            emits('api-error', error);
+        }
+
+    }
+
+    function updateSize(w: number, h: number) {
+        if (youtubePlayer.value) {
+            youtubePlayer.value.setSize(w, h);
+        }
+    }
+
+    function load (src: string, start: number) {
+        if (youtubePlayer.value) {
+            youtubePlayer.value.loadVideoById(src, start);
+        }
+    }
+
+    function attachReadyEvent() {
+        if (!window.onYouTubeIframeAPIReady) {
+            window.onYouTubeIframeAPIReady = () => {
+                resolve(true);
+                window.hbGlobalResolvers?.resolve(true);
+            };
+        }
+    }
+
+    function attachScriptToHeader(tag: HTMLScriptElement) {
+        const firstScriptTag = document.getElementsByTagName('script')[0];
+
+        // append iframe script as first script
         if (firstScriptTag && firstScriptTag.parentNode) {
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
         }
     }
-    else {
-        resolver();
+
+    function redownload(tag: HTMLScriptElement) {
+        tag.remove();
+
+        // Companion script for iframe_api
+        document.getElementById('www-widgetapi-script')?.remove();
+
+        delete window.yt_embedsEnableIframeApiSendFullEmbedUrl;
+        delete window.yt_embedsEnableAutoplayAndVisibilitySignals;
+        delete window.YTConfig;
+
+        // @ts-expect-error This should be cleared to be able to reload iframe
+        delete window['YT'];
+
+        attachScript();
     }
+
+    function createScriptTag(): HTMLScriptElement {
+
+        let tag = document.createElement('script');
+
+        tag.id = youtubeIframeScriptId;
+
+        tag.src = 'https://www.youtube.com/iframe_api'
+
+        tag.onerror = (e) => {
+
+            window.hbYoutubeIframeAttempts = Math
+                .max(0, (window.hbYoutubeIframeAttempts ?? 3) - 1);
+
+            redownload(tag);
+
+            if (window.hbYoutubeIframeAttempts < 1) {
+                console.error('failed to download iframe');
+
+                reject(new Error('YouTube Iframe API was not loaded'));
+                window.hbGlobalResolvers?.reject();
+            }
+        };
+
+        return tag;
+    }
+
+    async function attachScript() {
+        if (window.hbComponentLoading) {
+            try {
+                await window.hbGlobalResolvers?.promise;
+            }
+            catch (error) {
+                // nothing done here
+                return;
+            }
+        }
+
+        window.hbComponentLoading = true;
+
+        let widgetScript = document.getElementById(youtubeWidgetScriptId) as HTMLScriptElement
+
+        const propertyCheckList = [
+            window['yt_embedsEnableIframeApiSendFullEmbedUrl'],
+            window['yt_embedsEnableAutoplayAndVisibilitySignals'],
+            window['YT']?.loaded,
+            window['YTConfig'],
+        ].filter(Boolean).length >= 4;
+
+        if (widgetScript && propertyCheckList) {
+            return resolve(true);
+        }
+
+        let tag = createScriptTag();
+
+        attachScriptToHeader(tag);
+    }
+
+    return {
+        // Variables
+
+        player: youtubePlayer,
+
+        initiated,
+        ready,
+        youtubeIframeScriptId,
+
+        // Functions
+        attachReadyEvent,
+        attachScript,
+
+        createPlayer,
+
+        destroy,
+
+        load,
+        updateSize,
+    };
 }
 
-watch(() => props.width, () => {
-    player.value?.setSize(+props.width, +props.height);
+const Player = usePlayer();
+
+watch(
+    [() => props.width, () => props.height, () => props.src],
+    ([newWidth, newHeight, newSrc]) => {
+    Player.updateSize(+newWidth, +newHeight);
+    Player.load(newSrc, props.vars?.start ?? 0)
 });
 
-watch(() => props.height, () => {
-    player.value?.setSize(+props.width, +props.height);
+
+onBeforeMount(async () => {
+    Player.attachReadyEvent();
+    await Player.attachScript();
 });
 
-watch(() => props.src, () => {
-    if (initiated.value
-        && Boolean(player.value)
-        && Boolean(props.src)
-    ) {
-        player.value?.loadVideoById(props.src, props.vars?.start ?? 0);
-    }
-});
-
-onMounted(() => {
-    on_mounted(<HTMLElement>youtube.value);
+onMounted(async () => {
+    await Player.createPlayer();
 })
 
 onBeforeUnmount(() => {
-    player.value?.destroy();
+    Player.destroy();
 });
 
 defineExpose({
-    player: readonly(player),
-    initiated: readonly(initiated),
-    ready: readonly(ready),
-    ...useAPI({ player })
+    player: readonly(Player.player),
+    initiated: readonly(Player.initiated),
+    ready: readonly(Player.ready),
+    scriptId: readonly(ref(Player.youtubeIframeScriptId)),
+    ...useAPI({ player: Player.player })
 });
+
 </script>
